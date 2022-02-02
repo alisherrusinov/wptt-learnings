b51207c9f2cae1340b9f74416d82117a4ea6ce55
Alisher <alisher.nugmanoff@gmail.com>
2022-02-02 09:47:50 +0500

    php vars task

---
+++ php/001_variables.php
@@ -0,0 +1,18 @@
+<?php 
+$b = False;
+$isLessonCompleted = True;
+$nubmer1 = 5;
+$nubmer2 = 6;
+echo $nubmer1+$nubmer2;
+echo " ";
+echo gettype($nubmer2+$nubmer1);
+echo "<br>";
+$x = 7.3;
+$result = $x+$nubmer1;
+echo $result;
+$line1 = "string 1";
+$line2 = "string 3";
+$line3 = "string 2";
+echo "<br>";
+echo $line2.$line1.$line3;
+?>
